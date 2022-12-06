f7d5b89c0dcb241ad2e911bad896f1d847a38c59
Kevin <kjfeng@uw.edu>
2022-12-06 01:01:05 -0800

    related work updates

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-    
+    $(".citation").each(function() {
+        let id = $(this).attr("id");
+        $(this).append(`
+            [<a class="bib" data-trigger="hover" data-toggle="popover" data-placement="top" href="#bib` + id + `" data-original-title="" title="">` + id + `</a>]`);
+    });
 });
