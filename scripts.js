daa1df94a821bba67d7fd1648b1cc277bceb78d6
Kevin <kjfeng@uw.edu>
2022-12-07 02:20:45 -0800

    two vis + methods

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ function overviewBarChart() {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
         let [year, chiCount, neuripsCount] = [[], [], []];
 
         for (let i = 0; i < data.length; i++) {
@@ -36,7 +35,8 @@ function overviewBarChart() {
         var data = [dataChi, dataNeurips];
         var layout = {
             barmode: 'group',
-            xaxis: {autotick: false},
+            xaxis: {autotick: false, title: "Year"},
+            yaxis: {title: "# of Papers"},
             legend: {"orientation": "h", bgcolor: 'transparent', y: -0.2},
             title: {text: "Publication Output in CHI and NeurIPS, 2002-2021"}
         };
@@ -45,48 +45,46 @@ function overviewBarChart() {
     })
 }
 
-function similarityHeatmap() {
-    fetch("https://raw.githubusercontent.com/rrrrrrockpang/rrrrrrockpang.github.io/main/overview.json")
+function heatmapTitles() {
+    fetch("https://raw.githubusercontent.com/rrrrrrockpang/rrrrrrockpang.github.io/main/heatmap.json")
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        let [year, chiCount, neuripsCount] = [[], [], []];
+        
+        let [z, cYears, nYears] = [[], [], []];
 
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].conference === "CHI") {
-                chiCount.push(data[i].number);
-                year.push(data[i].year);
+        for (let i = 0; i < data.length; i+=18) {
+            zRow = [];
+            for (let j = 0; j < 18; j++) {
+                zRow.push(data[i+j].score);
             }
-
-            if (data[i].conference === "NeurIPS") {
-                neuripsCount.push(data[i].number);
-            }
+            z.push(zRow);
 
         }
-
-        var dataChi = {
-            x: year,
-            y: chiCount,
-            name: '# of CHI Papers',
-            type: 'bar'
-        };
-        var dataNeurips = {
-            x: year,
-            y: neuripsCount,
-            name: '# of NeurIPS Papers',
-            type: 'bar'
-        };
-        var data = [dataChi, dataNeurips];
+        for (let i = 2001; i < 2021; i++) {
+            cYears.push(i);
+            if (i < 2019) {
+                nYears.push(i);
+            }
+        }
+        console.log(z);
+        var data = [{
+            z: z,
+            x: nYears,
+            y: cYears,
+            type: 'heatmap',
+            hoverongaps: false,
+            colorscale: 'YlGnBu',
+        }];
+       
         var layout = {
-            barmode: 'group',
-            xaxis: {autotick: false},
-            legend: {"orientation": "h", bgcolor: 'transparent', y: -0.2},
-            title: {text: "Publication Output in CHI and NeurIPS, 2002-2021"}
+            title: {text: "Cosine Similarity Between CHI and NeurIPS Paper Titles, 2002-2021"},
+            xaxis: {autotick: false, title: "NeurIPS Year"},
+            yaxis: {autotick: false, title: "CHI Year"},
         };
 
-        Plotly.newPlot('overviewBarChart', data, layout);
+        Plotly.newPlot('heatmapTitles', data, layout);
     })
 }
 
@@ -99,6 +97,7 @@ $(document).ready(function() {
     });
 
     overviewBarChart();
+    heatmapTitles();
 
 });
 
